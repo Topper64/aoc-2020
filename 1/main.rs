8e7d602079232f0160bed784b61e5f9ef509210a
Topper64 <24857330+Topper64@users.noreply.github.com>
2020-12-04 21:56:24 +0000

    Do 1.2

--- 1/main.rs
+++ 1/main.rs
@@ -6,15 +6,26 @@ fn main() -> io::Result<()> {
     let reader = BufReader::new(file);
 
     let mut numbers = Vec::new();
+    let mut pairs = Vec::new();
     for line in reader.lines() {
         // Parse as a number
         let n = line?.parse::<i32>().unwrap();
 
-        // Check all previous numbers
-        for m in &numbers {
-            if m + n == 2020 {
-                println!("{}", m * n);
+        // Check all possible sums of 3 numbers
+        for (sum, prod) in &pairs {
+            if sum + n == 2020 {
+                println!("Part 2: {}", prod * n);
             }
+        }
+
+        // Update rolling sums and products
+        for m in &numbers {
+            let sum = m + n;
+            let prod = m * n;
+            if sum == 2020 {
+                println!("Part 1: {}", prod);
+            }
+            pairs.push((sum, prod));
         }
 
         // Remember for future loops
