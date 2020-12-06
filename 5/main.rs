01581a5072399191fa5f6156ce1f357a2cc53ef0
Topper64 <24857330+Topper64@users.noreply.github.com>
2020-12-06 18:37:22 +0000

    Do 5.2

--- 5/main.rs
+++ 5/main.rs
@@ -1,10 +1,12 @@
 use std::io::{self, BufRead, BufReader};
 use std::fs::File;
+use std::cmp::Ordering::{Less, Greater};
 
 fn main() -> io::Result<()> {
     let reader = BufReader::new(File::open("input.txt")?);
 
-    let highest = reader.lines()
+    // Read file
+    let mut nums: Vec<usize> = reader.lines()
         .map(|line| line.unwrap().chars().fold(0, |id, char|
             (id << 1) + match char {
                 'B' => 1,
@@ -12,8 +14,20 @@ fn main() -> io::Result<()> {
                 _ => 0,
             }
         ))
-        .max().unwrap();
-    println!("Part 1: {}", highest);
+        .collect();
+
+    // Since we're missing a single value, there is a point where the parity of a value compared
+    // to its index will change: find it with a binary search
+    nums.sort();
+    let parity = nums[0] & 1;
+    let pairs: Vec<_> = nums.iter().enumerate().collect();
+    let i = pairs.binary_search_by(|(i, n)| match (i ^ *n ^ parity) & 1 {
+        0 => Less,
+        _ => Greater,
+    }).err().unwrap();
+
+    println!("Part 1: {}", nums.last().unwrap());
+    println!("Part 2: {}", nums[i] - 1);
 
     Ok(())
 }
