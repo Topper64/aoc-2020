bc52b927aafcca7c8240806d918d139d4ed13aab
Topper64 <24857330+Topper64@users.noreply.github.com>
2020-12-06 19:46:14 +0000

    Do 6.2

--- 6/main.rs
+++ 6/main.rs
@@ -1,21 +1,28 @@
 use std::io::{self, BufRead, BufReader};
 use std::fs::File;
-use std::collections::HashSet;
+use std::collections::HashMap;
 
 fn main() -> io::Result<()> {
     let reader = BufReader::new(File::open("input.txt")?);
 
     let mut lines = reader.lines().map(|line| line.unwrap()).peekable();
-    let mut count = 0;
+    let mut count1 = 0;
+    let mut count2 = 0;
     while lines.peek().is_some() {
-        let mut answers: HashSet<char> = HashSet::new();
+        let mut answers: HashMap<_, _> = HashMap::new();
+        let mut npeople = 0;
         for line in (&mut lines).take_while(|line| line.len() > 0) {
-            answers.extend(line.chars());
+            npeople += 1;
+            for char in line.chars() {
+                *answers.entry(char).or_insert(0) += 1;
+            }
         }
-        count += answers.len();
+        count1 += answers.len();
+        count2 += answers.values().filter(|ans| *ans == &npeople).count();
     }
 
-    println!("Part 1: {}", count);
+    println!("Part 1: {}", count1);
+    println!("Part 2: {}", count2);
 
     Ok(())
 }
