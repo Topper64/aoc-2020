ea0d933d2dea6d88ce6cf0074f582405ec051555
Topper64 <24857330+Topper64@users.noreply.github.com>
2020-12-08 09:23:24 +0000

    Do 7.2

--- 7/main.rs
+++ 7/main.rs
@@ -12,6 +12,7 @@ fn main() -> io::Result<()> {
 
         let mut container = None;
         let mut colour_words = Vec::new();
+        let mut count = None;
         for word in line.split_whitespace().map(|word| word.trim_matches(',')) {
             match word {
                 // The word "bag" or "bags" marks the end of a colour
@@ -19,17 +20,19 @@ fn main() -> io::Result<()> {
                     let colour = colour_words.join(" ");
                     colour_words.clear();
                     if let Some(key) = &container {
-                        relations.entry(String::from(key))
-                            .or_insert(HashSet::new())
-                            .insert(colour);
+                        let contains = relations.entry(String::from(key))
+                            .or_insert(HashMap::new());
+                        if count > Some(0) {
+                            contains.insert(colour, count.unwrap());
+                        }
                     } else {
                         container = Some(colour);
                     }
                 },
 
                 // Numbers
-                "no" => (),
-                _ if word.chars().all(|char| char.is_ascii_digit()) => (),
+                "no" => count = Some(0),
+                _ if word.chars().all(|char| char.is_ascii_digit()) => count = word.parse().ok(),
 
                 // This always appears so adds no information
                 "contain" | "contains" => (),
@@ -47,7 +50,7 @@ fn main() -> io::Result<()> {
     while match size {None => true, Some(n) => has_gold.len() > n} {
         size = Some(has_gold.len());
         for (outer, inners) in relations.iter() {
-            if inners.iter().any(|colour| has_gold.contains(colour)) {
+            if inners.keys().any(|colour| has_gold.contains(colour)) {
                 has_gold.insert(String::from(outer));
             }
         }
@@ -56,7 +59,39 @@ fn main() -> io::Result<()> {
     // Subtract 1: the shiny gold bag itself
     let size = size.unwrap() - 1;
 
+    // Count how many total bags each bag contains
+    let mut counts = HashMap::new();
+    let mut done = false;
+    while !done {
+        done = true;
+
+        // Identify all the ones that we've counted all children of
+        let mut empty = Vec::new();
+        for (inner, inners) in relations.iter() {
+            if inners.is_empty() {
+                empty.push(String::from(inner));
+            }
+        }
+
+        for inner in empty.iter() {
+            // Remove it so we don't check again next time round
+            relations.remove(inner);
+
+            // Ensure this colour has a count
+            let count = *counts.entry(String::from(inner)).or_insert(0u64);
+
+            // Update the counts of anything containing it
+            for (outer, inners) in relations.iter_mut() {
+                if let Some(n) = inners.remove(inner) {
+                    *counts.entry(String::from(outer)).or_insert(0) += (count + 1) * n;
+                    done = false;
+                }
+            }
+        }
+    }
+
     println!("Part 1: {}", size);
+    println!("Part 2: {}", counts.get("shiny gold").unwrap());
 
     Ok(())
 }
