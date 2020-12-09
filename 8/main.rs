63908ae34692d83038275619a270bb27adf8f85e
Topper64 <24857330+Topper64@users.noreply.github.com>
2020-12-09 01:09:16 +0000

    Do 8.2

--- 8/main.rs
+++ 8/main.rs
@@ -1,46 +1,90 @@
 use std::io::{self, BufRead, BufReader};
 use std::fs::File;
 
+type Int = i32;
+type Instructions = Vec<(String, Int)>;
+
+fn check(instructions: &Instructions, corrupted: Option<usize>) -> Result<Int, Int> {
+    let mut visited = vec![false; instructions.len()];
+
+    let len = instructions.len();
+    let mut index = 0;
+    let mut acc = 0;
+    while index < len && !visited[index] {
+        visited[index] = true;
+
+        let (operation, argument) = &instructions[index];
+        let operation = operation.as_str();
+
+        // Apply corruption
+        let operation = match corrupted == Some(index) {
+            true => match operation {
+                "jmp" => "nop",
+                "nop" => "jmp",
+                _ => operation,
+            }
+            _ => operation,
+        };
+
+        // Carry out instruction
+        match operation {
+            "acc" => {
+                acc += argument;
+                index += 1;
+            },
+
+            "jmp" => {
+                let offset = *argument;
+                if offset > 0 {
+                    index += offset as usize;
+                } else {
+                    let offset = -offset as usize;
+                    if index < offset {
+                        break;
+                    }
+                    index -= offset;
+                }
+            },
+
+            "nop" => index += 1,
+
+            _ => panic!("idk dude"),
+        }
+    }
+
+    if index == len {
+        Ok(acc)
+    } else {
+        Err(acc)
+    }
+}
+
+fn try_fix(instructions: &Instructions) -> Option<Int> {
+    for i in 0..instructions.len() {
+        match check(&instructions, Some(i)) {
+            Ok(acc) => return Some(acc),
+            _ => (),
+        };
+    }
+    None
+}
+
 fn main() -> io::Result<()> {
     let reader = BufReader::new(File::open("input.txt")?);
 
     // Read file
     let mut instructions = Vec::new();
-    let mut visited = Vec::new();
     for line in reader.lines().map(|line| line.unwrap()) {
         let mut parts = line.split_whitespace();
 
         let operation = String::from(parts.next().unwrap());
-        let argument: i32 = parts.next().unwrap().parse().unwrap();
+        let argument: Int = parts.next().unwrap().parse().unwrap();
 
         instructions.push((operation, argument));
-        visited.push(false);
     }
 
-    let mut index = 0;
-    let mut acc = 0;
-    while !visited[index] {
-        visited[index] = true;
-
-        let (operation, argument) = &instructions[index];
-        match operation.as_str() {
-            "acc" => {
-                acc += argument;
-                index += 1;
-            },
-            "jmp" => {
-                if argument > &0 {
-                    index += *argument as usize;
-                } else {
-                    index -= -*argument as usize;
-                }
-            },
-            "nop" => index += 1,
-            _ => panic!("idk dude"),
-        }
-    }
-
-    println!("Part 1: {}", acc);
+    println!("Part 1: {}", check(&instructions, None).unwrap_err());
+    println!("Part 2: {}", try_fix(&instructions).unwrap());
 
     Ok(())
 }
