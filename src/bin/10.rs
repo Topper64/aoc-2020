bc43ceba9df399947f9cb5e8d641c8d4124a66ac
Topper64 <24857330+Topper64@users.noreply.github.com>
2020-12-10 09:15:19 +0000

    Do 10.2

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -5,7 +5,7 @@ fn main() -> io::Result<()> {
     let reader = BufReader::new(File::open("inputs/10.txt")?);
 
     // Read file
-    let mut nums: Vec<usize> = reader.lines()
+    let mut nums: Vec<_> = reader.lines()
         .map(|line| line.unwrap().parse().unwrap())
         .collect();
 
@@ -19,7 +19,19 @@ fn main() -> io::Result<()> {
     let ones = diffs.iter().filter(|d| **d == 1).count();
     let threes = diffs.iter().filter(|d| **d == 3).count();
 
+    // Count the ways of making a valid chain
+    let mut ways: Vec<u64> = vec![0; nums.len()];
+    ways[0] = 1;
+    for i in 0..ways.len() {
+        let a = nums[i];
+        let reachable = nums.iter().skip(i+1).take_while(|b| *b - a <= 3).count();
+        for j in i+1..i+1+reachable {
+            ways[j] += ways[i];
+        }
+    }
+
     println!("Part 1: {}", ones * threes);
+    println!("Part 2: {}", ways.last().unwrap());
 
     Ok(())
 }
