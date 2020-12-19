fb6710148cb933fe3615b885b935d7e400a054a1
Topper64 <24857330+Topper64@users.noreply.github.com>
2020-12-19 10:26:10 +0000

    Do 14.2

--- src/bin/14.rs
+++ src/bin/14.rs
@@ -8,32 +8,49 @@ fn main() -> io::Result<()> {
     // Read file
     let mut mask: u64 = 0;
     let mut default: u64 = 0;
-    let mut mem = HashMap::new();
+    let mut options = Vec::new();
+    let mut mem1 = HashMap::new();
+    let mut mem2 = HashMap::new();
     for line in reader.lines().map(|line| line.unwrap()) {
         let mut parts = line.split(" = ");
         let key = parts.next().unwrap();
         let value = parts.next().unwrap();
+
         if key == "mask" {
             mask = 0;
             default = 0;
+            options.clear();
+            options.push(0u64);
+
             for char in value.chars() {
                 mask <<= 1;
                 default <<= 1;
-                match char {
-                    'X' => mask += 1,
-                    '1' => default += 1,
-                    _ => {},
+                for option in options.iter_mut() {
+                    *option <<= 1;
+                }
+
+                if char == 'X' {
+                    mask += 1;
+                    let newopts: Vec<_> = options.iter().map(|opt| opt + 1).collect();
+                    options.extend(newopts);
+                } else if char == '1' {
+                    default += 1;
                 }
             }
         } else {
             // This looks like "mem[\d+]" but we don't really need regex to pick out the number
             let key: String = key.chars().filter(|c| c.is_ascii_digit()).collect();
+            let key: u64 = key.parse().unwrap();
             let value: u64 = value.parse().unwrap();
-            mem.insert(key, default + (value & mask));
+            mem1.insert(key, default | (value & mask));
+            for option in &options {
+                mem2.insert(default | option | (key & !mask), value);
+            }
         }
     }
 
-    println!("Part 1: {}", mem.values().sum::<u64>());
+    println!("Part 1: {}", mem1.values().sum::<u64>());
+    println!("Part 2: {}", mem2.values().sum::<u64>());
 
     Ok(())
 }
