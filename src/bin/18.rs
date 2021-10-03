f759916156ac1e5d91e818bb36c5974c77de760e
Topper64 <24857330+Topper64@users.noreply.github.com>
2021-10-03 16:26:09 +0100

    Do 18.2

--- src/bin/18.rs
+++ src/bin/18.rs
@@ -1,12 +1,16 @@
 use std::io::{self, BufRead, BufReader};
 use std::fs::File;
 
-enum Token {
-    Num(i64),
+enum Op {
     Add,
     Mul,
     Open,
     Close,
+}
+
+enum Token {
+    Num(i64),
+    Op(Op),
 }
 
 fn tokenise(expr: &str) -> Vec<Token> {
@@ -17,7 +21,7 @@ fn tokenise(expr: &str) -> Vec<Token> {
         // remembered for later
         while let Some(trimmed) = term.strip_prefix('(') {
             term = trimmed;
-            tokens.push(Token::Open);
+            tokens.push(Token::Op(Op::Open));
         }
         let mut close = 0;
         while let Some(trimmed) = term.strip_suffix(')') {
@@ -27,31 +31,35 @@ fn tokenise(expr: &str) -> Vec<Token> {
 
         // Identify operators or numbers
         tokens.push(match term {
-            "+" => Token::Add,
-            "*" => Token::Mul,
+            "+" => Token::Op(Op::Add),
+            "*" => Token::Op(Op::Mul),
             _ => Token::Num(i64::from_str_radix(term, 10).unwrap()),
         });
 
         // Add the closing brackets
         while close > 0 {
             close -= 1;
-            tokens.push(Token::Close);
+            tokens.push(Token::Op(Op::Close));
         }
     }
     tokens
 }
 
-fn evaluate(expr: &str) -> i64 {
+fn evaluate<F, T>(expr: &str, prec: F) -> i64
+where
+    F: Fn(&Op) -> T,
+    T: PartialOrd,
+{
     let mut nums = Vec::new();
     let mut ops = Vec::new();
 
     // Function to apply an operator to the top two numbers
-    let eval = |nums: &mut Vec<i64>, op: &Token| {
+    let eval = |nums: &mut Vec<i64>, op: &Op| {
         let n1 = nums.pop().unwrap();
         let n2 = nums.pop().unwrap();
         nums.push(match op {
-            Token::Add => n1 + n2,
-            Token::Mul => n1 * n2,
+            Op::Add => n1 + n2,
+            Op::Mul => n1 * n2,
             _ => panic!(),
         });
     };
@@ -60,27 +68,29 @@ fn evaluate(expr: &str) -> i64 {
     for token in tokens.iter() {
         match token {
             Token::Num(n) => nums.push(*n),
-            Token::Open => ops.push(token),
-            Token::Close => {
+            Token::Op(Op::Open) => ops.push(&Op::Open),
+            Token::Op(Op::Close) => {
                 // Evaluate all operators back to the matching open bracket
                 while let Some(op) = ops.pop() {
-                    if let Token::Open = op {
+                    if let Op::Open = op {
                         break;
                     }
                     eval(&mut nums, op);
                 }
             },
-            _ => {
+            Token::Op(op) => {
                 // Evaluate operators with higher or equal precedence
-                while let Some(op) = ops.pop() {
-                    if let Token::Open = op {
-                        ops.push(op);
+                let p = prec(op);
+                while let Some(prev) = ops.pop() {
+                    if prec(prev) >= p {
+                        eval(&mut nums, prev);
+                    } else {
+                        ops.push(prev);
                         break;
                     }
-                    eval(&mut nums, op);
                 }
                 // Remember current operator
-                ops.push(token);
+                ops.push(op);
             },
         }
     }
@@ -93,18 +103,38 @@ fn evaluate(expr: &str) -> i64 {
     nums.pop().unwrap()
 }
 
+// No precedence: a always precedes (or is equal to) b
+fn no_prec(a: &Op) -> i8 {
+    match a {
+        Op::Open => -1,  // Must be lowest
+        Op::Close => 1,  // Irrelevant, might as well be highest
+        _ => 0,
+    }
+}
+
+// Addition preceeds multiplication
+fn add_mul(a: &Op) -> i8 {
+    match a {
+        Op::Open => -1,
+        Op::Mul => 1,
+        Op::Add => 2,
+        Op::Close => 10,
+    }
+}
+
 fn main() -> io::Result<()> {
     let reader = BufReader::new(File::open("inputs/18.txt")?);
 
     // Read file
-    let mut tot = 0;
+    let mut part1 = 0;
+    let mut part2 = 0;
     for line in reader.lines().map(|line| line.unwrap()) {
-        let ans = evaluate(&line);
-        tot += ans;
-        // println!("{} = {}", line, ans);
+        part1 += evaluate(&line, no_prec);
+        part2 += evaluate(&line, add_mul);
     }
 
-    println!("Part 1: {}", tot);
+    println!("Part 1: {}", part1);
+    println!("Part 2: {}", part2);
 
     Ok(())
 }
